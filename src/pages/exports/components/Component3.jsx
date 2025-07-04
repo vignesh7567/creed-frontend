a42899409659b5d2f5034d3553074d7213151124
yogesh-guvi-2913 <yogeshwar@guvi.in>
2025-07-04 15:38:57 +0530

    Added responsive to export page

--- src/pages/exports/components/Component3.jsx
+++ src/pages/exports/components/Component3.jsx
@@ -4,11 +4,11 @@ import export2 from '../../../assets/exports/export2.png'
 
 const Component3 = () => {
   return (
-    <div className=' flex h-[100%] bg-blue-600 justify-between items-start mt-[20px] md:mt-[100px]'>
-      <div className=' w-[40%] bg-green-400 h-full flex justify-start items-center'>
+    <div className=' flex justify-between items-center md:items-start mt-[20px] md:mt-[100px]'>
+      <div className=' w-[50%] md:w-[40%] h-auto flex justify-center items-center'>
         <img className=' w-[450px]' src={export2} alt='export1' />
       </div>
-      <div className=' w-[60%] bg-red-500'>
+      <div className=' w-[50%] md:w-[60%]'>
         <div className='text-[20px] md:text-[40px] font-[800] mb-[10px] md:mb-[50px]'>Available Fabric Options:</div>
         <ul className=' list-disc pl-1 md:pl-5 text-[12px] md:text-[24px] font-[500] text-[rgba(0,0,0,0.7)]'>
           <li className=' pb-[2px] md:pb-[20px]'><span className=' font-[600] text-gray-800'>Cotton –</span> Soft, breathable, and durable</li>
--- src/pages/exports/components/Component4.jsx
+++ src/pages/exports/components/Component4.jsx
@@ -4,17 +4,17 @@ import export3 from '../../../assets/exports/export3.png'
 
 const Component4 = () => {
   return (
-    <div className=' flex justify-between items-start mt-[100px] mb-[200px]'>
-      <div className=' w-[60%]'>
-        <div className='text-[40px] font-[800] mb-[50px]'>Why Choose The Creed for Exports?</div>
-        <ul className=' list-disc pl-5 text-[24px] font-[500] text-[rgba(0,0,0,0.7)]'>
-          <li className=' pb-[20px]'>Central export hub in Dubai with access to global shipping</li>
-          <li className=' pb-[20px]'>Premium finish, scalable quantities, and quick turnarounds</li>
-          <li className=' pb-[20px]'>Fully customizable — fabric, fit, print, branding, and packaging</li>
-          <li className=' pb-[20px]'>End-to-end logistics support from documentation to dispatch</li>
+    <div className=' flex justify-between items-center md:items-start mt-[50px] md:mt-[100px] md:mb-[200px]'>
+      <div className=' w-[60%] md:w-[60%]'>
+        <div className='text-[20px] md:text-[40px] font-[800] md:mb-[50px]'>Why Choose The Creed for Exports?</div>
+        <ul className=' list-disc pl-1 md:pl-5 text-[12px] md:text-[24px] font-[500] text-[rgba(0,0,0,0.7)]'>
+          <li className='pb-[2px] md:pb-[20px]'>Central export hub in Dubai with access to global shipping</li>
+          <li className='pb-[2px] md:pb-[20px]'>Premium finish, scalable quantities, and quick turnarounds</li>
+          <li className='pb-[2px] md:pb-[20px]'>Fully customizable — fabric, fit, print, branding, and packaging</li>
+          <li className='pb-[2px] md:pb-[20px]'>End-to-end logistics support from documentation to dispatch</li>
         </ul>
       </div>
-      <div className=' w-[40%] flex justify-end'>
+      <div className=' w-[40%] md;w-[40%] flex justify-end'>
         <img className=' w-[450px]' src={export3} alt='export1' />
       </div>
     </div>

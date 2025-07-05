aa55353b67337975053d68f99f041177dc0bba0f
yogesh-guvi-2913 <yogeshwar@guvi.in>
2025-07-05 10:56:16 +0530

    Added link in redirect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
 
 const Header = () => {
@@ -9,12 +10,12 @@ const Header = () => {
         <img src={logo} alt="Logo" className=' w-[59px] md:w-[164px]' />
       </div>
       <div className=' flex gap-[14px] md:gap-[27px] text-[8px] sm:text-[16px] font-[600] text-white items-center'>
-        <a className=' hidden md:block hover:text-red-400 transition-transform duration-200 hover:scale-105' href='/'>HOME</a>
-        <a href='/about' className=' hover:text-red-400 transition-transform duration-200 hover:scale-105'>ABOUT</a>
-        <a href='/#services' className=' hover:text-red-400 transition-transform duration-200 hover:scale-105' >OUR SERVICES</a>
-        <a href='/#products' className=' hover:text-red-400 transition-transform duration-200 hover:scale-105' >PRODUCTS</a>
-        <a className=' hidden md:block hover:text-red-400 transition-transform duration-200 hover:scale-105' href='/#client' >OUR CLIENTS</a>
-        <a href='/contact' className=' hover:text-red-400 transition-transform duration-200 hover:scale-105'>CONTACT US</a>
+        <Link className=' hidden md:block hover:text-red-400 transition-transform duration-200 hover:scale-105' to='/'>HOME</Link>
+        <Link to='/about' className=' hover:text-red-400 transition-transform duration-200 hover:scale-105'>ABOUT</Link>
+        <Link to='/#services' className=' hover:text-red-400 transition-transform duration-200 hover:scale-105' >OUR SERVICES</Link>
+        <Link to='/#products' className=' hover:text-red-400 transition-transform duration-200 hover:scale-105' >PRODUCTS</Link>
+        <Link tosName=' hidden md:block hover:text-red-400 transition-transform duration-200 hover:scale-105' href='/#client' >OUR CLIENTS</Link>
+        <Link to='/contact' className=' hover:text-red-400 transition-transform duration-200 hover:scale-105'>CONTACT US</Link>
       </div>
     </div>
   )
